3644f7deee6892b7c35e33c8cba890079859ed97
Pragyan <pragyan52yadav@gmail.com>
2022-04-14 18:15:13 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+    return (
+        <div className="bg-black text-white">
+            <div className="container mx-auto min-h-screen flex justify-center items-center text-center font-bold text-3xl md:text-5xl">
+                NextJS + TailwindCSS Repository Template
+            </div>
+        </div>
+    );
+};
+
+export default Index;
