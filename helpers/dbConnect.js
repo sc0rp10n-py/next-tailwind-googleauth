9d1cb5394638872839a0a64b0946750234ff60e9
sc0rp10n-lappy-wslubuntu <pragyan52yadav@gmail.com>
2023-01-08 07:04:52 +0530

    mongodb connect update

--- helpers/dbConnect.js
+++ helpers/dbConnect.js
@@ -1,40 +1,16 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
+const dbConnect = handler => async (req, res) => {
+    if (mongoose.connections[0].readyState) {
+        // Use current db connection
+        return handler(req, res);
+    }
+    // Use new db connection
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+    return handler(req, res);
+    }
 
 export default dbConnect;
